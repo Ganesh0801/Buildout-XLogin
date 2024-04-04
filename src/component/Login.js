bd5f13dac8da5b43f8c7d31a78e80469ec35ee86
Ganesh <ganeshsbsa08@gmail.com>
2024-04-04 20:27:38 +0530

    first commit

---
+++ src/component/Login.js
@@ -0,0 +1,67 @@
+import React,{useState} from 'react';
+
+
+const Login = () => {
+    const[name,setName] = useState("");
+    const[password,setPassword]=useState("");
+    const[login,setLogin]=useState(false);
+   const[warn,setWarn]=useState(false);
+ 
+    const handleName = (e)=>{
+        setName(e.target.value)
+        console.log("name clicked")
+    }
+    const handlePass = (e)=>{
+        setPassword(e.target.value)
+        console.log("pass type")
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+       if(name === "user" && password === "password"){
+        setLogin(true)
+        setWarn(false)
+       }
+       else{
+        console.log("not");
+        setWarn(true)
+       }
+    }
+
+  return (
+   <>
+     <h1>Login Page</h1>
+     
+     {login ? <p>Welcome user ! </p> :(
+        <form style={{display:"block"}} >
+             {warn && <p>Invalid username or password</p>}
+        <div>
+        <label htmlFor='username'>Username:</label>
+        <input 
+        id='username'
+        type='text'
+        value={name}
+        onChange={handleName}
+        required
+        />
+        </div>
+       <div>
+       <label htmlFor='password'>Password:</label>
+        <input 
+        id='password'
+        type='password'
+        value={password}
+        onChange={handlePass}
+        required
+        />
+       </div>
+       <button type='submit'
+       onSubmit={handleSubmit}>Submit</button>
+     </form>
+     )}
+     
+   </>
+  )
+}
+
+export default Login;
